62024658193cb3e0587ced62962997b91522495f
Ali Ansari <badnan541@gmail.com>
2026-09-12 21:59:29 +0100

    Add history record ID generation with duplicate checking

--- app/history_manager.py
+++ app/history_manager.py
@@ -1,6 +1,22 @@
+import random
 
 class HistoryManager:
     MAX_HISTORY_ENTRIES = 10
 
     def __init__(self):
         self.record_history = []
+
+    def generate_record_id(self):
+        while True:
+            record_id = str(
+                random.randint(100000000000, 999999999999)
+            )
+
+            duplicate_found = False
+            for record in self.record_history:
+                if record["id"] == record_id:
+                    duplicate_found = True
+                    break
+
+            if not duplicate_found:
+                return record_id
